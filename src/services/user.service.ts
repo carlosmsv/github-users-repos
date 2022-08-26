1dae0f256aa159330b49b341eca9cebd03d82bc3
Carlos <carlos.msv@hotmail.com>
2022-08-26 16:14:51 -0300

    requests are now using api client id and secret

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -3,14 +3,15 @@ import { Injectable } from '@nestjs/common';
 import { User } from '../models/user';
 import { Repository } from '../models/repository';
 
-
+const githubClientId = process.env.GITHUB_CLIENT_ID;
+const githubClientSecret = process.env.GITHUB_CLIENT_SECRET;
 @Injectable()
 export class UserService {
 
     private readonly url = 'https://api.github.com/users';
 
     public async listUsers(since: number): Promise<User[]> {
-      const usersUrl = `${this.url}?since=${since}&per_page=9`;
+      const usersUrl = `${this.url}?since=${since}&per_page=9?client_id=${githubClientId}&client_secret=${githubClientSecret}`;
 
       const users: User[] = await axios.get(usersUrl).then(response => {
           return response.data;
@@ -20,7 +21,7 @@ export class UserService {
     }
 
     public async getUser(login: string): Promise<User> {
-      const userUrl = `${this.url}/${login}`;
+      const userUrl = `${this.url}/${login}?client_id=${githubClientId}&client_secret=${githubClientSecret}`;
 
       const user: User = await axios.get(userUrl).then(response => {
           return response.data;
@@ -31,7 +32,7 @@ export class UserService {
 
   public async listRepositories(login: string): Promise<Repository[]> {
 
-    const reposUrl = `${this.url}/${login}/repos`;
+    const reposUrl = `${this.url}/${login}/repos?client_id=${githubClientId}&client_secret=${githubClientSecret}`;
 
     const repositories: Repository[] = await axios.get(reposUrl).then(response => {
         return response.data;
