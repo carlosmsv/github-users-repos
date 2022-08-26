d5035a426ad4fa01e02307039871c62812dfd161
Carlos <carlos.msv@hotmail.com>
2022-08-25 22:02:02 -0300

    models

---
+++ src/models/repository.ts
@@ -0,0 +1,5 @@
+export class Repository {
+  public id: number;
+  public name: string;
+  public html_url: string;
+}
---
+++ src/models/user.ts
@@ -0,0 +1,8 @@
+export class User {
+  public login: string;
+  public id: number;
+  public avatar_url: string;
+  public html_url: string;
+  public repos_url: string;
+  public created_at: string;
+}
