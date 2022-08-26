cf10a9e61e8f7ad498cfe180cbfcf257156ec990
Carlos <carlos.msv@hotmail.com>
2022-08-25 22:23:49 -0300

    testing list users since randomNumber

--- src/controllers/users.controller.spec.ts
+++ src/controllers/users.controller.spec.ts
@@ -10,6 +10,12 @@ describe('User Controller', () => {
     query: {},
   } as unknown as Request
 
+  let randomNumber = Math.floor(Math.random() * 10000);
+
+  const requestMockWithRandomNumber = {
+    query: {since: randomNumber},
+  } as unknown as Request
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
@@ -23,11 +29,20 @@ describe('User Controller', () => {
     expect(controller).toBeDefined();
   });
 
-  it('should be able to get users list, 9 at a time', () => {
+  it('should be able to list users, 9 at a time', () => {
     return controller.list(requestMock)
     .then((list) => {
       expect(list).toHaveLength;
       expect(list.length).toBe(9);
+    })
+  })
+  
+  it('should be able to list users, 9 at a time, starting from ', () => {
+    return controller.list(requestMockWithRandomNumber)
+    .then((list) => {
+      expect(list).toHaveLength;
+      expect(list.length).toBe(9);
+      expect(list[0].id).toBeGreaterThan(randomNumber)
     })
   })
 });
